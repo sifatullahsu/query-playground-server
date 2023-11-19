7a5173e1d7d39afdab968f368ad518bbc96a6405
sifatullahsu <personal.sifat@gmail.com>
2023-11-19 18:06:42 +0600

    slugMaker

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,6 @@
 import apiResponse from './files/apiResponse'
 import catchAsync from './files/catchAsync'
 import { createToken, verifyToken } from './files/jwtHelper'
+import slugMaker from './files/slugMaker'
 
-export { apiResponse, catchAsync, createToken, verifyToken }
+export { apiResponse, catchAsync, createToken, slugMaker, verifyToken }
